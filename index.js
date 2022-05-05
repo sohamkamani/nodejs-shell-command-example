fd846d506ad5622ee2d1db79066605a6fe260716
sohamkamani <sohamkamani@gmail.com>
2022-05-05 18:37:03 +0530

    added comments

--- index.js
+++ index.js
@@ -15,39 +15,56 @@ function basicShellCommand() {
 }
 
 function longRunningCommand() {
+    // start the `ping google.com` command
     const command = spawn('ping', ["google.com"])
+    // const command = spawn('ping', ["google.com"])
+
+    // the `data` event is fired every time data is
+    // output from the command
     command.stdout.on('data', output => {
+        // the output data is captured and printed in the callback
         console.log("Output: ", output.toString())
     })
 }
 
 function spawnStdin() {
+    // run the grep command
     const command = spawn('grep', ["apple"])
 
+    // use the stdin stream from the command to
+    // send data to the spawned command
     command.stdin.write("1. pear\n")
     command.stdin.write("2. grapes\n")
     command.stdin.write("3. apple\n")
     command.stdin.write("4. banana\n")
+    // once we're done sending input, call the `end` method
     command.stdin.end()
 
+    // similar to the previous example, print the output whenever it's 
+    // received
     command.stdout.on('data', output => {
         console.log("Output: ", output.toString())
     })
 }
 
 function kill() {
+    // execute the sleep command - this will
+    // wait for 100 seconds before exiting
     const command = exec("sleep 100")
 
+    // when a child process exits, it fires
+    // the "close" event
     command.on('close', (code) => {
         console.log('process has exited')
     })
 
+    // Since we don't want to wait for 100 seconds,
+    // we can send a kill command after a 1 second timeout
     setTimeout(() => {
         command.kill()
-
     }, 1000)
 
 
 }
 
-kill()
+longRunningCommand()
